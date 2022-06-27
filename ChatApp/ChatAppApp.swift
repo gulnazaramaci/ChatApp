793f8f342ad0216671f41b77463c4d388483e63d
Gülnaz Aramacı <78848288+glnzaramaci@users.noreply.github.com>
2022-06-27 16:00:13 +0300

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Gülnaz Aramaci on 27.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
